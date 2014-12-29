19deefbfcd16b977676fc9abbab6045c4c62cbf6
Brady Salz <brady.salz@gmail.com>
2014-12-29 16:03:30 -0500

    jQuery saves the day... ruh roh

--- website/plots/rc-filter.m
+++ website/plots/rc-filter.m
@@ -1,32 +1,8 @@
-% 
-figure()
-A = csvread('../spice/rc-filter-1.csv');
-plot(A(:,1), A(:,2))
-xlabel('Frequency (Hz)')
-ylabel('Amplitude (V/V)')
-title('RC Filter Output')
-pause
-
-figure()
-A = csvread('../spice/rc-filter-1.csv');
-semilogx(A(:,1), A(:,2))
-xlabel('Frequency (Hz)')
-ylabel('Amplitude (V/V)')
-title('RC Filter Output')
-pause
-
-figure()
 A = csvread('../spice/rc-filter-2.csv');
-plot(A(:,1), A(:,2))
+h = semilogx(A(:,1), A(:,2))
 xlabel('Frequency (Hz)')
 ylabel('Amplitude (dB)')
 title('RC Filter Output')
-pause
-
-figure()
-A = csvread('../spice/rc-filter-2.csv');
-semilogx(A(:,1), A(:,2))
-xlabel('Frequency (Hz)')
-ylabel('Amplitude (dB)')
-title('RC Filter Output')
+grid on;
+print('../static/images/plots/rc-filter.svg', '-dsvg')
 pause
