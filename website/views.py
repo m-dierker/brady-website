7dd91fb573ccbff935b75e18db8394ad19d43333
Brady Salz <brady.salz@gmail.com>
2014-12-29 00:28:57 -0500

    i've fallen in love with a beautiful goddess called octave

--- website/views.py
+++ website/views.py
@@ -13,30 +13,39 @@ from prettifySPICE import *
 @app.route('/index.html')
 @app.route('/home')
 @app.route('/')
-def home():
-    return render_template('aboutme.html', title='About')
-
 @app.route('/about')
 def about():
-	return render_template('aboutme.html', title='About')
+    return render_template('aboutme.html', title='About')
 
-@app.route('/post')
-def post():
-	cat = Category.query.all()
-	cat = cat[0]
-	bodypath = ("/home/brady/website/website/posts/resistor-divider.html")
-	postbody = open(bodypath, 'r').read()
-	testPost = Post(title='Resistor Divider', body=postbody, category=cat)
-	postbody = render_template_string(postbody)
-	return render_template('post.html', post=testPost, body=postbody, title=testPost.title)
-
-@app.route('/shivani')
-def shivani():
-	return render_template('aboutugly.html', title='Ugly')
 
 @app.route('/resume')
 def resume():
 	return render_template('resume.html')
+
+@app.errorhandler(404)
+def pageNotFound(e):
+	return render_template('error.html', title='404', errmsg='Page Not Found')
+
+@app.route('/post/<post>')
+def anyPost(post):
+	validPath = os.path.dirname(__file__)
+	relPath = 'posts'
+	validPath = os.path.join(validPath, relPath)
+
+	validPosts = [ f for f in os.listdir(validPath) if os.path.isfile(os.path.join(validPath, f)) ]
+	validPosts = [ p.rsplit('.')[0] for p in validPosts ]
+
+	if post in validPosts:
+		validFile = os.path.join(validPath, post + '.json')
+		jsonData = open(validFile, 'r')
+		jsonData = json.load(jsonData, strict=False)
+		rBody = render_template_string(jsonData['body'])
+		return render_template('post.html', title=jsonData['title'], date=jsonData['date'],
+								category=jsonData['category'], body=rBody)
+
+
+	else:
+		return pageNotFound(404)
 
 @app.route('/blogtest')
 def blogex():
@@ -61,26 +70,7 @@ def plotsample():
 	plotTest = open(plotTest, 'r').read()
 	return render_template('codesample.html', data=plotTest)
 
-@app.route('/post/<post>')
-def anyPost(post):
-	validPath = os.path.dirname(__file__)
-	relPath = 'posts'
-	validPath = os.path.join(validPath, relPath)
 
-	validPosts = [ f for f in os.listdir(validPath) if os.path.isfile(os.path.join(validPath, f)) ]
-	validPosts = [ p.rsplit('.')[0] for p in validPosts ]
-
-	if post in validPosts:
-		validFile = os.path.join(validPath, post + '.json')
-		jsonData = open(validFile, 'r')
-		jsonData = json.load(jsonData, strict=False)
-		rBody = render_template_string(jsonData['body'])
-		return render_template('post.html', title=jsonData['title'], date=jsonData['date'],
-								category=jsonData['category'], body=rBody)
-
-
-	#return ' '.join(validPosts)
-
-
-
-
+@app.route('/shivani')
+def shivani():
+	return render_template('aboutugly.html', title='Ugly')
