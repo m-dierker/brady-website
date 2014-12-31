52b2dd1c782f79eee0d7047b326977979aaa746a
Matthew Dierker <mvd7793@gmail.com>
2014-12-30 19:24:24 -0600

    add note to brady about a line that does not work

--- addpost.py
+++ addpost.py
@@ -7,6 +7,8 @@ newCat = input('Enter category (passive, active, rf, ms, power, digital): ')
 newContent = open(newFile, 'r').read()
 
 cat = models.Category.query.filter_by(name=newCat).first()
+# TODO(bradysalz): My IDE highlights this next as not working because it cannot work.
+# (Post is not imported)
 newPost = Post(title=newTitle, body=newContent, cat=cat)
 
 models.db.session.add(newPost)
